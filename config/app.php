4f73c14882e0ed33aa75cc7a0c06c25015df8ad3
Alexandr Pushkin <118882770+push-ab@users.noreply.github.com>
2025-11-05 16:19:28 +0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'alexandr-pushkin/alexandr-pushkin'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
